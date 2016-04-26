09e106c1a8a54830dac582f2f480cbb521e68238
DAN YU <danyu1996@gmail.com>
2016-04-25 23:55:21 -0700

    finished files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# autocomplete
-A Clone of an Auto-Complete Web Widget Built for a Meteor Coding Challenge
+Auto Complete Clone
+===================
+
+This jquery plugin is a clone of the auto complete featured jquery plugin found here: https://jqueryui.com/autocomplete/. This plugin was created as part of a Meteor Coding Challenge.
+
+
+
+Table of Contents
+-----------------
+
+- [Getting Started](#getting-started)
+- [License](#license)
+
+
+
+Getting Started
+---------------
+
+Get started by first cloning the repository:
+
+```bash
+# Get the latest version:
+$ git clone https://github.com/danXyu/autocomplete.git
+$ cd autocomplete
+$ open test/index.html
+```
+
+
+
+License
+-------
+
+The MIT License (MIT)
+
+Copyright (c) 2014-2015 Dan Yu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/jquery.autocomplete.js
@@ -0,0 +1,112 @@
+/*	Project: autocomplete
+ *  Description: A clone of an autocomplete searching implementation
+ *  Author: Dan Yu
+ *  License: MIT
+ */
+
+(function($, window, document) {
+	"use strict";
+	var pluginName = "autocomplete";
+
+	/* Constructor: Autocomplete
+	 * -------------------------
+	 * Creates reference to DOM element, and stores information
+	 * necessary for creating the autocomplete searchable list.
+	 */
+	function Autocomplete(element, options) {
+		this.element = element;
+		this.settings = $.extend({}, options);
+		this._name = pluginName;
+		this.init();
+	}
+
+	// Avoid Plugin.prototype conflicts
+	$.extend(Autocomplete.prototype, {
+
+		/* Prototype Function: init
+		 * ------------------------
+		 * Init function for Autocomplete. Ensures that the necessary
+		 * DOM elements are created, with event listeners created too.
+		 */
+		init: function() {
+			var thisReference = this
+			var $autocomplete = $('<div>', {id:'autocomplete'});
+
+			var $searchTitle = $('<label>', {text: 'Search Box:', id:'search-title'});
+			$autocomplete.append($searchTitle);
+
+			var $searchBox = $('<input>', {id: 'search-box'});
+			$searchBox.bind('input', function() {
+				thisReference.autocomplete($searchBox.val());
+			});
+			$autocomplete.append($searchBox);
+
+			var $search = $('<span />').attr('class', 'glyphicon glyphicon-arrow-right')
+			$search.bind('click', function(event) {
+				thisReference.animateMeteors();
+			});
+			$autocomplete.append($search);
+
+			var $searchResults = $('<ul>', {id: 'search-results'});
+			$autocomplete.append($searchResults);
+
+			$(this.element).append($autocomplete);
+		},
+
+		/* Prototype Function: autocomplete
+		 * --------------------------------
+		 * Filter case insensitively an array of strings based on a given search query,
+		 * and update the UI to reflect the filtered array, allow search bar filling-in.
+		 *
+		 * Arguments:
+		 * searchQuery - text entered into the search box
+		 */
+		autocomplete: function(searchQuery) {
+			var filteredSearch = _.filter(this.settings.searchArray, function (searchItem) {
+				return searchQuery.length != 0 && searchItem.toLowerCase().indexOf(searchQuery.toLowerCase()) != -1
+			});
+
+			$('#search-results').empty();
+			_.each(filteredSearch, function(filteredItem, index) {
+				var $searchResult = $('<li>', {class: 'search-result-'+index, text: filteredItem});
+				$('#search-results').append($searchResult);
+			})
+
+			$('#search-results').bind('click', function(event) {
+				$('#search-box').val(event.target.innerText);
+				$('#search-results').empty();
+			});
+		},
+
+		/* Prototype Function: autocomplete
+		 * --------------------------------
+		 * Random added feature, adding in a meteor shower whenever right arrow is clicked.
+		 */
+		animateMeteors: function() {
+			var thisReference = this
+			var meteors = []
+
+			_.each(_.range(15), function(meteor) {
+				var $meteor = $('<img />', {src: '../src/public/meteor_icon.png'});
+				$meteor.css('position', 'absolute');
+				$meteor.css('top', '-'+String(Math.random()*1000+100)+'px');
+				$meteor.css('left', String(Math.random()<0.5?'-':'')+String(Math.random()*2000)+'px');
+				meteors.push($meteor);
+			});
+
+			_.each(meteors, function(meteor) {
+				$(thisReference.element).append(meteor);
+				$(meteor).animate({left: '+=3000', top: '+=3000'}, 4000);
+			});
+		}
+	});
+
+	// Ensure that the plugin is only loaded once by the browser.
+	$.fn[pluginName] = function(options) {
+		return this.each(function() {
+			if (!$.data(this, "plugin_" + pluginName)) {
+				$.data(this, "plugin_" + pluginName, new Autocomplete(this, options));
+			}
+		});
+	};
+})(jQuery, window, document);
